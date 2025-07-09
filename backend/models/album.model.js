92971c07a60650b12b603d5760d468f3c98d5f12
Rahul <rahulp00rtx@gmail.com>
2025-07-09 20:57:58 +0530

    Created the Schema and models

---
+++ backend/models/album.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const albumSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    artist: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    relaseYear: {
+      type: Number,
+      required: true,
+    },
+    song: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Song",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Album = mongoose.model("Album", albumSchema);
