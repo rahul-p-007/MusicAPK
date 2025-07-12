a3b3e1f4d473666800921e17b672b18bdaa56db3
Rahul <rahulp00rtx@gmail.com>
2025-07-13 01:35:22 +0530

    build the static portion of the AlbumPage and also made the types ..........for type safety

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage.tsx";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 import Mainlayout from "./layout/Mainlayout.tsx";
 import ChatPage from "./pages/chat/ChatPage.tsx";
+import AlbumPage from "./pages/album/AlbumPage.tsx";
 function App() {
   return (
     <Routes>
@@ -21,6 +22,7 @@ function App() {
       <Route element={<Mainlayout />}>
         <Route path="/" element={<HomePage />} />
         <Route path="/chat" element={<ChatPage />} />
+        <Route path="/albums/:albumId" element={<AlbumPage />} />
       </Route>
     </Routes>
   );
--- frontend/src/stores/useMusicStore.ts
+++ frontend/src/stores/useMusicStore.ts
@@ -1,36 +1,47 @@
 import { axiosInstance } from "@/lib/axios";
 import type { Album, Song } from "@/types";
 import { create } from "zustand";
+
 interface MusicStore {
   songs: Song[];
-
   albums: Album[];
+  currentAlbum: Album | null;
   isLoading: boolean;
   error: string | null;
   fetchAlbums: () => Promise<void>;
+  fetchAlbumById: (id: string) => Promise<void>;
 }
+
 export const useMusicStore = create<MusicStore>((set) => ({
-  albums: [],
   songs: [],
+  albums: [],
+  currentAlbum: null,
   isLoading: false,
   error: null,
+
   fetchAlbums: async () => {
-    set({
-      isLoading: true,
-      error: null,
-    });
+    set({ isLoading: true, error: null });
     try {
       const response = await axiosInstance.get("/albums");
       set({ albums: response.data });
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (error: any) {
-      set({
-        error: error.response.data.message,
-      });
+      set({ error: error.response?.data?.message || "Failed to fetch albums" });
     } finally {
-      set({
-        isLoading: false,
-      });
+      set({ isLoading: false });
+    }
+  },
+
+  fetchAlbumById: async (id: string) => {
+    set({ isLoading: true, error: null });
+    try {
+      const response = await axiosInstance.get(`/albums/${id}`);
+      set({ currentAlbum: response.data });
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      set({ error: error.response?.data?.message || "Failed to fetch album" });
+    } finally {
+      set({ isLoading: false });
     }
   },
 }));
