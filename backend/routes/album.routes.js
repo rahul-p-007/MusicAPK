929485db70a006f591dad96e7b6841e351ffe992
Rahul <rahulp00rtx@gmail.com>
2025-07-17 14:20:44 +0530

    --Last-commit

--- backend/routes/album.routes.js
+++ backend/routes/album.routes.js
@@ -4,4 +4,7 @@ const route = Router();
 
 route.get("/", getAllAlbums);
 route.get("/:albumId", getAlbumById);
+console.log("Registering /:albumId route");
+// route.get("/:albumId", getAlbumById);
+
 export default route;
