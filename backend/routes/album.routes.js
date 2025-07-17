a638cbaef3ea7b9a33aa8c4d6c5020c1aed6493d
Rahul <rahulp00rtx@gmail.com>
2025-07-17 14:17:46 +0530

    --Date--

--- backend/routes/album.routes.js
+++ backend/routes/album.routes.js
@@ -2,8 +2,6 @@ import { Router } from "express";
 import { getAllAlbums, getAlbumById } from "../controllers/album.controller.js";
 const route = Router();
 
-// route.get("/", getAllAlbums);
 route.get("/", getAllAlbums);
-// route.get("/:albumId", getAlbumById);
 route.get("/:albumId", getAlbumById);
 export default route;
